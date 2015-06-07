f7a55396d463a281e29bd01521c56b3c601fb8b8
Markus Ahrweiler <Markus.Ahrweiler@web.de>
2015-06-07 09:51:20 +0200

    reformat code

--- swipe/latest.js
+++ swipe/latest.js
@@ -1,125 +1,125 @@
-( function ( window, $ ) {
+(function ( window, $ ) {
 
-	Fancy.require ( {
-		jQuery: false,
-		Fancy: "1.1.0"
-	} );
+    Fancy.require ( {
+        jQuery: false,
+        Fancy : "1.1.0"
+    } );
 
-	function getType ( e ) {
-		var t = e.originalEvent.touches,
-		    r = e.originalEvent.changedTouches;
-		t = t ? t [ 0 ] : false;
-		r = r ? r [ 0 ] : false;
-		if ( t ) {
-			return {
-				x: t.clientX,
-				y: t.clientY
-			};
-		} else if ( r ) {
-			return {
-				x: r.clientX,
-				y: r.clientY
-			};
-		} else {
-			return {
-				x: e.clientX,
-				y: e.clientY
-			};
-		}
+    function getType ( e ) {
+        var t = e.originalEvent.touches,
+            r = e.originalEvent.changedTouches;
+        t     = t ? t [ 0 ] : false;
+        r     = r ? r [ 0 ] : false;
+        if ( t ) {
+            return {
+                x: t.clientX,
+                y: t.clientY
+            };
+        } else if ( r ) {
+            return {
+                x: r.clientX,
+                y: r.clientY
+            };
+        } else {
+            return {
+                x: e.clientX,
+                y: e.clientY
+            };
+        }
 
-	}
+    }
 
-	function preventSelect ( e ) {
-		e.attr ( "unselectable", "on" ).css ( "user-select", "none" ).on ( "selectstart", false );
-	}
+    function preventSelect ( e ) {
+        e.attr ( "unselectable", "on" ).css ( "user-select", "none" ).on ( "selectstart", false );
+    }
 
-	function enableSelect ( e ) {
-		e.removeAttr ( "unselectable" ).css ( "user-select", "" ).unbind ( "selectstart" );
-	}
+    function enableSelect ( e ) {
+        e.removeAttr ( "unselectable" ).css ( "user-select", "" ).unbind ( "selectstart" );
+    }
 
-	var NAME = "FancySwipe",
-	    VERSION = "2.1.0",
-	    logged = false;
+    var NAME    = "FancySwipe",
+        VERSION = "2.1.0",
+        logged  = false;
 
-	function FancySwipe ( element, settings ) {
-		var SELF = this;
-		SELF.name = NAME;
-		SELF.version = VERSION;
-		SELF.element = element;
-		SELF.settings = $.extend ( {}, Fancy.settings [ NAME ], settings );
+    function FancySwipe ( element, settings ) {
+        var SELF      = this;
+        SELF.name     = NAME;
+        SELF.version  = VERSION;
+        SELF.element  = element;
+        SELF.settings = $.extend ( {}, Fancy.settings [ NAME ], settings );
 
-		if ( !logged ) {
-			logged = true;
-			Fancy.version ( SELF );
-		}
-		SELF.element.on ( 'mousedown touchstart', function ( e ) {
-			var startX = getType ( e ).x,
-			    dir = '',
-			    startY = getType ( e ).y,
-			    marginX,
-			    marginY;
-			preventSelect ( $ ( document ) );
-			SELF.settings.onMouseDown.call ( SELF.element, startX, startX );
-			$ ( document ).on ( 'mousemove.' + NAME + ' touchmove.' + NAME, function ( e ) {
-				var x = getType ( e ).x,
-				    y = getType ( e ).y;
-				//                        moving right
-				if ( x > startX ) {
-					marginX = x - startX;
-					if ( marginX > SELF.settings.min )
-						dir = 'right';
-				} else if ( x < startX ) {
-					//                            moving left
-					marginX = startX - x;
-					if ( marginX > SELF.settings.min )
-						dir = 'left';
-				}
-				if ( y > startY ) {
-					//                            moving down
-					marginY = y - startY;
-					if ( marginY > SELF.settings.min )
-						dir = ( dir && dir != 'down' ? dir + ' ' : '' ) + 'down';
-				} else if ( y < startY ) {
-					//                            moving up
-					marginY = startY - y;
-					if ( marginY > SELF.settings.min )
-						dir = ( dir && dir != 'up' ? dir + ' ' : '' ) + 'up';
-				}
-				SELF.settings.onMouseMove.call ( SELF.element, e, dir, marginX, marginY );
-			} ).on ( 'mouseup.' + NAME + ' touchend.' + NAME, function ( e ) {
-				enableSelect ( $ ( document ).unbind ( '.' + NAME ) );
-				if ( marginX > SELF.settings.min && ( SELF.settings.max ? marginX < SELF.settings.max : true ) ) {
-					SELF.settings.onMouseUp.call ( SELF.element, e, dir, marginX, marginY );
-				} else if ( marginY > SELF.settings.min && ( SELF.settings.max ? marginY < SELF.settings.max : true ) ) {
-					SELF.settings.onMouseUp.call ( SELF.element, e, dir, marginX, marginY );
-				}
-			} );
-		} ).on ( "remove", function ( ) {
-			enableSelect ( $ ( document ).unbind ( '.' + NAME ) );
-		} );
-		SELF.element.data ( NAME, SELF );
-		return SELF;
-	}
+        if ( !logged ) {
+            logged = true;
+            Fancy.version ( SELF );
+        }
+        SELF.element.on ( 'mousedown touchstart', function ( e ) {
+            var startX = getType ( e ).x,
+                dir    = '',
+                startY = getType ( e ).y,
+                marginX,
+                marginY;
+            preventSelect ( $ ( document ) );
+            SELF.settings.onMouseDown.call ( SELF.element, startX, startX );
+            $ ( document ).on ( 'mousemove.' + NAME + ' touchmove.' + NAME, function ( e ) {
+                var x = getType ( e ).x,
+                    y = getType ( e ).y;
+                //                        moving right
+                if ( x > startX ) {
+                    marginX = x - startX;
+                    if ( marginX > SELF.settings.min )
+                        dir = 'right';
+                } else if ( x < startX ) {
+                    //                            moving left
+                    marginX = startX - x;
+                    if ( marginX > SELF.settings.min )
+                        dir = 'left';
+                }
+                if ( y > startY ) {
+                    //                            moving down
+                    marginY = y - startY;
+                    if ( marginY > SELF.settings.min )
+                        dir = ( dir && dir != 'down' ? dir + ' ' : '' ) + 'down';
+                } else if ( y < startY ) {
+                    //                            moving up
+                    marginY = startY - y;
+                    if ( marginY > SELF.settings.min )
+                        dir = ( dir && dir != 'up' ? dir + ' ' : '' ) + 'up';
+                }
+                SELF.settings.onMouseMove.call ( SELF.element, e, dir, marginX, marginY );
+            } ).on ( 'mouseup.' + NAME + ' touchend.' + NAME, function ( e ) {
+                enableSelect ( $ ( document ).unbind ( '.' + NAME ) );
+                if ( marginX > SELF.settings.min && ( SELF.settings.max ? marginX < SELF.settings.max : true ) ) {
+                    SELF.settings.onMouseUp.call ( SELF.element, e, dir, marginX, marginY );
+                } else if ( marginY > SELF.settings.min && ( SELF.settings.max ? marginY < SELF.settings.max : true ) ) {
+                    SELF.settings.onMouseUp.call ( SELF.element, e, dir, marginX, marginY );
+                }
+            } );
+        } ).on ( "remove", function () {
+            enableSelect ( $ ( document ).unbind ( '.' + NAME ) );
+        } );
+        SELF.element.data ( NAME, SELF );
+        return SELF;
+    }
 
 
-	FancySwipe.api = FancySwipe.prototype = {};
-	FancySwipe.api.version = VERSION;
-	FancySwipe.api.name = NAME;
+    FancySwipe.api = FancySwipe.prototype = {};
+    FancySwipe.api.version = VERSION;
+    FancySwipe.api.name    = NAME;
 
-	Fancy.settings [ NAME ] = {
-		min: false,
-		max: false,
-		onMouseUp: function ( ) {
-		},
-		onMouseMove: function ( ) {
-		},
-		onMouseDown: function ( ) {
-		}
+    Fancy.settings [ NAME ] = {
+        min        : false,
+        max        : false,
+        onMouseUp  : function () {
+        },
+        onMouseMove: function () {
+        },
+        onMouseDown: function () {
+        }
 
-	};
+    };
 
-	Fancy.swipe = true;
-	Fancy.api.swipe = function ( settings ) {
-		return this.set ( FancySwipe, settings );
-	};
-} ) ( window, jQuery );
+    Fancy.swipe     = true;
+    Fancy.api.swipe = function ( settings ) {
+        return this.set ( FancySwipe, settings );
+    };
+}) ( window, jQuery );
--- switch/latest.js
+++ switch/latest.js
@@ -1,123 +1,123 @@
-(function (window, $) {
+(function ( window, $ ) {
 
-    Fancy.require({
+    Fancy.require ( {
         jQuery: false,
-        Fancy: "1.1.0"
-    });
-    var NAME = "FancySwitch",
+        Fancy : "1.1.0"
+    } );
+    var NAME    = "FancySwitch",
         VERSION = "2.1.0",
-        logged = false;
+        logged  = false;
 
-    function preventSelect(el) {
-        return el.on("selectstart", false).attr('unselectable', "on").css("userSelect", "none");
+    function preventSelect ( el ) {
+        return el.on ( "selectstart", false ).attr ( 'unselectable', "on" ).css ( "userSelect", "none" );
     }
 
-    function FancySwitch(element, settings) {
+    function FancySwitch ( element, settings ) {
         var SELF = this;
 
-        SELF.settings = $.extend({}, Fancy.settings[NAME], settings);
-        SELF.visible = false;
-        if (!logged) {
+        SELF.settings = $.extend ( {}, Fancy.settings[ NAME ], settings );
+        SELF.visible  = false;
+        if ( !logged ) {
             logged = true;
-            Fancy.version(SELF);
+            Fancy.version ( SELF );
         }
-        SELF.element = element;
-        SELF.version = VERSION;
-        SELF.name = NAME;
+        SELF.element   = element;
+        SELF.version   = VERSION;
+        SELF.name      = NAME;
         SELF.animating = false;
-        SELF.element.addClass(NAME + "-element");
-        SELF.items = SELF.element.find(SELF.settings.itemSelector);
+        SELF.element.addClass ( NAME + "-element" );
+        SELF.items     = SELF.element.find ( SELF.settings.itemSelector );
 
-        if (SELF.settings.drag) {
-            SELF.element.addClass(NAME + "-draggable");
+        if ( SELF.settings.drag ) {
+            SELF.element.addClass ( NAME + "-draggable" );
         }
 
-        SELF.items.each(function (i) {
-            var item = $(this);
-            item.data("position", item.css("position") === "static" ? "" : item.css("position"));
-            item.data("top", item.css("top") === "auto" ? "" : item.css("top"));
+        SELF.items.each ( function ( i ) {
+            var item = $ ( this );
+            item.data ( "position", item.css ( "position" ) === "static" ? "" : item.css ( "position" ) );
+            item.data ( "top", item.css ( "top" ) === "auto" ? "" : item.css ( "top" ) );
             var $el;
-            if (!item.find(SELF.settings.upSelector).length && !SELF.settings.drag) {
-                if (SELF.settings.upSelector.indexOf(".") === 0) {
-                    $el = $("<div/>", {
-                        class: SELF.settings.upSelector.substr(1)
-                    });
+            if ( !item.find ( SELF.settings.upSelector ).length && !SELF.settings.drag ) {
+                if ( SELF.settings.upSelector.indexOf ( "." ) === 0 ) {
+                    $el = $ ( "<div/>", {
+                        class: SELF.settings.upSelector.substr ( 1 )
+                    } );
                 } else {
-                    $el = $("<" + SELF.settings.upSelector + "/>");
+                    $el = $ ( "<" + SELF.settings.upSelector + "/>" );
                 }
-                $el.addClass(SELF.settings.upClass).html(SELF.settings.upText);
+                $el.addClass ( SELF.settings.upClass ).html ( SELF.settings.upText );
 
-                item.append($el);
-            } else if (SELF.settings.drag) {
-                item.find(SELF.settings.upSelector).remove();
+                item.append ( $el );
+            } else if ( SELF.settings.drag ) {
+                item.find ( SELF.settings.upSelector ).remove ();
             }
-            if (!item.find(SELF.settings.downSelector).length && !SELF.settings.drag) {
-                if (SELF.settings.downSelector.indexOf(".") === 0) {
-                    $el = $("<div/>", {
-                        class: SELF.settings.downSelector.substr(1)
-                    });
+            if ( !item.find ( SELF.settings.downSelector ).length && !SELF.settings.drag ) {
+                if ( SELF.settings.downSelector.indexOf ( "." ) === 0 ) {
+                    $el = $ ( "<div/>", {
+                        class: SELF.settings.downSelector.substr ( 1 )
+                    } );
                 } else {
-                    $el = $("<" + SELF.settings.downSelector + "/>");
+                    $el = $ ( "<" + SELF.settings.downSelector + "/>" );
                 }
-                $el.addClass(SELF.settings.downClass).html(SELF.settings.downText);
+                $el.addClass ( SELF.settings.downClass ).html ( SELF.settings.downText );
 
-                item.append($el);
-            } else if (SELF.settings.drag) {
-                item.find(SELF.settings.downSelector).remove();
+                item.append ( $el );
+            } else if ( SELF.settings.drag ) {
+                item.find ( SELF.settings.downSelector ).remove ();
             }
 
-            if (!SELF.settings.drag) {
-                preventSelect(item.find(SELF.settings.upSelector)).click(function () {
-                    SELF.up(item);
-                });
+            if ( !SELF.settings.drag ) {
+                preventSelect ( item.find ( SELF.settings.upSelector ) ).click ( function () {
+                    SELF.up ( item );
+                } );
 
-                preventSelect(item.find(SELF.settings.downSelector)).click(function () {
-                    SELF.down(item);
-                });
+                preventSelect ( item.find ( SELF.settings.downSelector ) ).click ( function () {
+                    SELF.down ( item );
+                } );
             } else {
-                item.addClass(NAME + "-draggable-item");
+                item.addClass ( NAME + "-draggable-item" );
                 var offset;
-                preventSelect(item).on("mousedown", function (e) {
+                preventSelect ( item ).on ( "mousedown", function ( e ) {
                     var clone;
-                    if (e.which === 1) {
+                    if ( e.which === 1 ) {
                         offset = {
                             x: e.offsetX,
                             y: e.offsetY
                         }
-                        $(document).on("mousemove." + NAME, function (event) {
-                            if (!clone) {
-                                clone = item.clone();
-                                $("body").append(clone);
-                                clone.css({
-                                    width: item.width(),
-                                    zIndex: (parseInt(item.css("zIndex")) || 10) + 1
-                                }).addClass(NAME + "-draggable-clone");
+                        $ ( document ).on ( "mousemove." + NAME, function ( event ) {
+                            if ( !clone ) {
+                                clone = item.clone ();
+                                $ ( "body" ).append ( clone );
+                                clone.css ( {
+                                    width : item.width (),
+                                    zIndex: (parseInt ( item.css ( "zIndex" ) ) || 10) + 1
+                                } ).addClass ( NAME + "-draggable-clone" );
                             }
-                            clone.css({
-                                top: Math.min(SELF.items.last().position().top, Math.max(SELF.items.first().position().top, event.pageY - offset.y)),
-                                left: item.offset().left
-                            });
-                            if (clone.position().top > item.position().top + (item.height() / 3 * 2)) {
-                                SELF.down(item, false);
-                            } else if (clone.position().top + (item.height() / 3 * 2) < item.position().top) {
-                                SELF.up(item, false);
+                            clone.css ( {
+                                top : Math.min ( SELF.items.last ().position ().top, Math.max ( SELF.items.first ().position ().top, event.pageY - offset.y ) ),
+                                left: item.offset ().left
+                            } );
+                            if ( clone.position ().top > item.position ().top + (item.height () / 3 * 2) ) {
+                                SELF.down ( item, false );
+                            } else if ( clone.position ().top + (item.height () / 3 * 2) < item.position ().top ) {
+                                SELF.up ( item, false );
                             }
 
-                        }).on("mouseup." + NAME, function (event) {
-                            if (event.which === 1 && clone) {
-                                clone.animate({
-                                    top: item.position().top
-                                },300, function () {
-                                    clone.remove();
-                                });
-                                $(document).off("." + NAME);
+                        } ).on ( "mouseup." + NAME, function ( event ) {
+                            if ( event.which === 1 && clone ) {
+                                clone.animate ( {
+                                    top: item.position ().top
+                                }, 300, function () {
+                                    clone.remove ();
+                                } );
+                                $ ( document ).off ( "." + NAME );
                             }
-                        });
+                        } );
                     }
 
-                });
+                } );
             }
-        });
+        } );
 
         return SELF;
     }
@@ -125,84 +125,84 @@
 
     FancySwitch.api = FancySwitch.prototype = {};
     FancySwitch.api.version = VERSION;
-    FancySwitch.api.name = NAME;
-    FancySwitch.api.up = function (item, animated) {
+    FancySwitch.api.name    = NAME;
+    FancySwitch.api.up      = function ( item, animated ) {
         var SELF = this;
-        var prev = item.prev();
-        if (prev.length) {
-            if (animated === false || SELF.settings.animated === false) {
-                item.insertBefore(prev);
-                SELF.items = SELF.element.find(SELF.settings.itemSelector);
-            } else if (!SELF.animating) {
+        var prev = item.prev ();
+        if ( prev.length ) {
+            if ( animated === false || SELF.settings.animated === false ) {
+                item.insertBefore ( prev );
+                SELF.items = SELF.element.find ( SELF.settings.itemSelector );
+            } else if ( !SELF.animating ) {
                 SELF.animating = !SELF.animating;
-                item.css("position", "relative").animate({
-                    top: "-" + prev.height() + "px"
+                item.css ( "position", "relative" ).animate ( {
+                    top: "-" + prev.height () + "px"
                 }, 500, function () {
-                    item.css("position", item.data("position"));
-                    item.css("top", item.data("top"));
-                });
-                prev.css("position", "relative").animate({
-                    top: "+" + item.height() + "px"
+                    item.css ( "position", item.data ( "position" ) );
+                    item.css ( "top", item.data ( "top" ) );
+                } );
+                prev.css ( "position", "relative" ).animate ( {
+                    top: "+" + item.height () + "px"
                 }, 500, function () {
-                    prev.css("position", prev.data("position"));
-                    prev.css("top", prev.data("top"));
-                    item.insertBefore(prev);
+                    prev.css ( "position", prev.data ( "position" ) );
+                    prev.css ( "top", prev.data ( "top" ) );
+                    item.insertBefore ( prev );
                     SELF.animating = !SELF.animating;
-                    SELF.items = SELF.element.find(SELF.settings.itemSelector);
-                    SELF.settings.onChange.call(SELF);
-                });
+                    SELF.items     = SELF.element.find ( SELF.settings.itemSelector );
+                    SELF.settings.onChange.call ( SELF );
+                } );
             }
         }
 
     };
-    FancySwitch.api.down = function (item, animated) {
+    FancySwitch.api.down    = function ( item, animated ) {
         var SELF = this;
-        var next = item.next();
-        if (item.next().length) {
-            if (animated === false || SELF.settings.animated === false) {
-                item.insertAfter(next);
-                SELF.items = SELF.element.find(SELF.settings.itemSelector);
-            } else if (!SELF.animating) {
+        var next = item.next ();
+        if ( item.next ().length ) {
+            if ( animated === false || SELF.settings.animated === false ) {
+                item.insertAfter ( next );
+                SELF.items = SELF.element.find ( SELF.settings.itemSelector );
+            } else if ( !SELF.animating ) {
                 SELF.animating = !SELF.animating;
-                item.css("position", "relative").animate({
-                    top: "+" + next.height() + "px"
+                item.css ( "position", "relative" ).animate ( {
+                    top: "+" + next.height () + "px"
                 }, 500, function () {
-                    item.css("position", item.data("position"));
-                    item.css("top", item.data("top"));
-                });
-                next.css("position", "relative").animate({
-                    top: "-" + item.height() + "px"
+                    item.css ( "position", item.data ( "position" ) );
+                    item.css ( "top", item.data ( "top" ) );
+                } );
+                next.css ( "position", "relative" ).animate ( {
+                    top: "-" + item.height () + "px"
                 }, 500, function () {
-                    next.css("position", next.data("position"));
-                    next.css("top", next.data("top"));
-                    item.insertAfter(next);
+                    next.css ( "position", next.data ( "position" ) );
+                    next.css ( "top", next.data ( "top" ) );
+                    item.insertAfter ( next );
                     SELF.animating = !SELF.animating;
-                    SELF.items = SELF.element.find(SELF.settings.itemSelector);
-                    SELF.settings.onChange.call(SELF);
-                });
+                    SELF.items     = SELF.element.find ( SELF.settings.itemSelector );
+                    SELF.settings.onChange.call ( SELF );
+                } );
             }
         }
 
     };
 
-    Fancy.settings[NAME] = {
-        drag: false,
-        animated: true,
+    Fancy.settings[ NAME ] = {
+        drag        : false,
+        animated    : true,
         itemSelector: ".FancySwitch-item",
-        upSelector: ".FancySwitch-up",
+        upSelector  : ".FancySwitch-up",
         downSelector: ".FancySwitch-down",
-        upClass: "",
-        downClass: "",
-        upText: "Up",
-        downText: "Down",
-        onChange: function () {}
+        upClass     : "",
+        downClass   : "",
+        upText      : "Up",
+        downText    : "Down",
+        onChange    : function () {}
     };
 
     Fancy.
-    switch = true;
+        switch = true;
     Fancy.api.
-    switch = function (settings) {
-        return this.set(FancySwitch, settings);
+        switch = function ( settings ) {
+        return this.set ( FancySwitch, settings );
     };
 
-})(window, jQuery);
+}) ( window, jQuery );
