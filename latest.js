078bbdb34fccdec0961a0d2a79a48c560030925d
Markus Ahrweiler <Markus.Ahrweiler@web.de>
2015-06-13 11:00:13 +0200

    Version 1.0.1

--- latest.js
+++ latest.js
@@ -10,27 +10,84 @@
         return;
     }
 
-    function capitalize ( str ) {
-        console.log ( str );
-        return str [ 0 ].toUpperCase () + str.slice ( 1, str.length );
-    }
-
-    var logged = false;
+    var n = navigator.userAgent.toLowerCase ();
 
     function Fancy ( element ) {
         if ( this == window )
             return new Fancy ( element );
         this.element = $ ( element );
         this.name    = "Fancy";
-        if ( !logged ) {
-            logged = true;
-            Fancy.version ( this );
-        }
     }
+
+    Fancy.findByAnd    = function ( array, obj, returnIndex ) {
+        function findByAnd () {
+            var resolved = true,
+                i        = 0;
+            while ( i < this.length ) {
+                resolved = true;
+                for ( var a in obj ) {
+                    if ( obj.hasOwnProperty ( a ) ) {
+                        if ( (a.toLowerCase ().indexOf ( "date" ) >= 0 ? new Date ( Fancy.getKey ( this[ i ], a ) ).getTime () : Fancy.getKey ( this[ i ], a )) != (a.toLowerCase ().indexOf ( "date" ) >= 0 ? new Date ( obj[ a ] ).getTime () : obj[ a ]) ) {
+                            resolved = false;
+                            break;
+                        }
+                    }
+                }
+                if ( resolved == true ) {
+                    resolved = i;
+                    break;
+                }
+                i++;
+            }
+            resolved   = (resolved === true || resolved === false) ? null : resolved;
+            var result = resolved === null ? null : this[ resolved ];
+            return returnIndex ? { index: resolved, result: result } : result;
+        }
+
+        return findByAnd.apply ( array );
+    };
+    Fancy.findAllBy    = function ( arr, obj ) {
+        function findAllBy () {
+            var list     = [],
+                resolved = true;
+            this.forEach ( function ( it ) {
+                resolved = true;
+                for ( var a in obj ) {
+                    if ( obj.hasOwnProperty ( a ) ) {
+                        if ( (a.toLowerCase ().indexOf ( "date" ) >= 0 ? new Date ( Fancy.getKey ( this, a ) ).getTime () : Fancy.getKey ( this, a )) != (a.toLowerCase ().indexOf ( "date" ) >= 0 ? new Date ( obj[ a ] ).getTime () : obj[ a ]) ) {
+                            resolved = false;
+                        }
+                    }
+                }
+                if ( resolved == true ) {
+                    list.push ( it );
+                }
+            } );
+            return list;
+        }
+
+        return findAllBy.apply ( arr );
+    };
+    Fancy.JSONvalid    = function JSONvalid ( text ) {
+        return text && /^[\],:{}\s]*$/.test ( text.replace ( /\\["\\\/bfnrtu]/g, '@' ).replace ( /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']' ).replace ( /(?:^|:|,)(?:\s*\[)+/g, '' ) )
+    };
+    Fancy.isObject     = function isObject ( obj ) {
+        return {}.toString.apply ( obj ) === '[object Object]';
+    };
+    Fancy.isArray      = function isArray ( obj ) {
+        return Object.prototype.toString.call ( obj ) === '[object Array]';
+    };
+    Fancy.capitalize   = function capitalize ( str ) {
+        return str [ 0 ].toUpperCase () + str.slice ( 1 );
+    };
+    Fancy.decapitalize = function decapitalize ( str ) {
+        return str [ 0 ].toLowerCase () + str.slice ( 1 );
+    };
 
 
     Fancy.api = Fancy.prototype = {
-        version: "1.0.0"
+        version: "1.0.1",
+        name   : "Fancy"
     };
 
     Fancy.isOpera        = !!window.opera || navigator.userAgent.indexOf ( ' OPR/' ) >= 0;
@@ -38,12 +95,11 @@
     Fancy.isSafari       = Object.prototype.toString.call ( window.HTMLElement ).indexOf ( 'Constructor' ) > 0;
     Fancy.isChrome       = !!window.chrome && !this.isOpera;
     Fancy.isIE           = !!document.documentMode;
-    var n                = navigator.userAgent.toLowerCase ();
     Fancy.apple          = n.indexOf ( "iphone" ) > 0 || n.indexOf ( "ipad" ) > 0 || n.indexOf ( "ipod" ) > 0;
     Fancy.mobile         = n.indexOf ( "mobile" ) > 0 || Fancy.apple;
     Fancy.versionControl = true;
 
-    Fancy.version = function ( plugin ) {
+    Fancy.version        = function ( plugin ) {
         if ( Fancy.versionControl ) {
             if ( Fancy.isChrome ) {
                 console.log ( "%cThis page is using %c" + plugin.name + "%c\r\n Copyright \u00a9 %cMarkus Ahrweiler\r\n %cVersion: %c" + plugin.version, 'color: #000', 'color: #8E0000', 'color: #000', 'color: #49A54F', 'color: #000', 'color: blue' );
@@ -80,16 +136,16 @@
             } );
         }
     };
-
-    Fancy.require = function ( plugins ) {
+    Fancy.require        = function ( plugins ) {
         for ( var i in plugins ) {
-            var vers = window [ i ].prototype.version || ( i == "jQuery" ? jQuery.prototype.jquery : false );
-            if ( typeof window [ i ] == "undefined" || ( vers && plugins [ i ] && Fancy.compareversion ( plugins [ i ], vers ) ) ) {
-                throw "Error: " + i + " " + ( plugins [ i ] ? plugins [ i ] + " " : "" ) + "is required" + ( vers ? ", got " + vers : "" );
+            if ( plugins.hasOwnProperty ( i ) ) {
+                var vers = window [ i ].prototype.version || ( i == "jQuery" ? jQuery.prototype.jquery : false );
+                if ( typeof window [ i ] == "undefined" || ( vers && plugins [ i ] && Fancy.compareversion ( plugins [ i ], vers ) ) ) {
+                    throw "Error: " + i + " " + ( plugins [ i ] ? plugins [ i ] + " " : "" ) + "is required" + ( vers ? ", got " + vers : "" );
+                }
             }
         }
     };
-
     Fancy.compareversion = function ( needed, is ) {
         // returns true, if needed is greater than is;
         var c = [ parseInt ( needed.split ( "." ) [ 0 ] ), parseInt ( needed.split ( "." ) [ 1 ] ), parseInt ( needed.split ( "." ) [ 2 ] ) ],
@@ -97,104 +153,111 @@
 
         return c [ 0 ] > d [ 0 ] || ( c [ 0 ] == d [ 0 ] && c [ 1 ] > d [ 1 ] ) || ( c [ 1 ] == d [ 1 ] && c [ 2 ] > d [ 2 ] );
     };
-
-    Fancy.check = function ( s, p ) {
+    Fancy.check          = function ( s, p ) {
         for ( var i in p ) {
-            if ( p [ i ].type ) {
-                var r = true;
-                var t = p [ i ].type.split ( "|" );
-                for ( var a = 0; a < t.length; a++ ) {
-                    if ( r ) {
-                        var type = s [ i ] instanceof window [ t [ a ] ] || typeof s [ i ] === t [ a ].toLowerCase ();
-                        if ( type ) {
-                            r = false;
-                        }
-                    }
-                }
-                if ( r ) {
-                    throw "Error: Expected type " + t.join ( " or " ) + " but got " + capitalize ( typeof s [ i ] ) + " for " + i;
-                }
-            }
-
-            if ( p [ i ].required ) {
-                var r = false;
-                switch ( typeof s[ i ] ) {
-                    case "string":
-                        if ( !s [ i ] )
-                            r = true;
-                        break;
-                    case "object":
-                        if ( s [ i ] === null )
-                            r = true;
-                        else if ( !Object.keys ( s [ i ] ).length )
-                            r = true;
-                        break;
-                    case "undefined":
-                        r = true;
-                        break;
-                }
-                if ( r ) {
-                    throw "Error: " + i + " is required";
-                }
-            }
-
-            if ( p [ i ].types && s [ i ] && s [ i ].length ) {
-                for ( var b = 0; b < s [ i ].length; b++ ) {
-                    var t = p [ i ].types.split ( "|" ),
-                        r = false;
-                    for ( var a = 0; a < t.length; a++ ) {
-                        if ( !r ) {
-                            var type = s [ i ] [ b ] instanceof window [ t [ a ] ] || typeof s [ i ] [ b ] === t [ a ].toLowerCase ();
-
-                            if ( s [ i ] [ b ] === null && t [ a ] != "Null" ) {
-                                type = false;
-                            }
+            if ( p.hasOwnProperty ( i ) ) {
+                var r, t, a, type;
+                if ( p [ i ].type ) {
+                    r = true;
+                    t = p [ i ].type.split ( "|" );
+                    for ( a = 0; a < t.length; a++ ) {
+                        if ( r ) {
+                            type = s [ i ] instanceof window [ t [ a ] ] || typeof s [ i ] === t [ a ].toLowerCase ();
                             if ( type ) {
-                                r = true;
+                                r = false;
                             }
                         }
                     }
-                    if ( !r ) {
-                        throw "Error: Expected type " + t.join ( " or " ) + " but got " + capitalize ( s [ i ] [ b ] === null ? "null" : typeof s [ i ] [ b ] ) + " for " + i + "'s items";
+                    if ( r ) {
+                        throw "Error: Expected type " + t.join ( " or " ) + " but got " + capitalize ( typeof s [ i ] ) + " for " + i;
                     }
                 }
 
+                if ( p [ i ].required ) {
+                    r = false;
+                    switch ( typeof s[ i ] ) {
+                        case "string":
+                            if ( !s [ i ] )
+                                r = true;
+                            break;
+                        case "object":
+                            if ( s [ i ] === null )
+                                r = true;
+                            else if ( !Object.keys ( s [ i ] ).length )
+                                r = true;
+                            break;
+                        case "undefined":
+                            r = true;
+                            break;
+                    }
+                    if ( r ) {
+                        throw "Error: " + i + " is required";
+                    }
+                }
+
+                if ( p [ i ].types && s [ i ] && s [ i ].length ) {
+                    for ( var b = 0; b < s [ i ].length; b++ ) {
+                        t = p [ i ].types.split ( "|" );
+                        r = false;
+                        for ( a = 0; a < t.length; a++ ) {
+                            if ( !r ) {
+                                type = s [ i ] [ b ] instanceof window [ t [ a ] ] || typeof s [ i ] [ b ] === t [ a ].toLowerCase ();
+
+                                if ( s [ i ] [ b ] === null && t [ a ] != "Null" ) {
+                                    type = false;
+                                }
+                                if ( type ) {
+                                    r = true;
+                                }
+                            }
+                        }
+                        if ( !r ) {
+                            throw "Error: Expected type " + t.join ( " or " ) + " but got " + capitalize ( s [ i ] [ b ] === null ? "null" : typeof s [ i ] [ b ] ) + " for " + i + "'s items";
+                        }
+                    }
+
+                }
             }
 
         }
 
     };
-
-    Fancy.watch = function ( obj, prop, handler ) {
+    Fancy.watch          = function ( obj, prop, handler ) {
         var oldval = obj[ prop ], newval = oldval,
             getter = function () {
                 return newval;
             },
             setter = function ( val ) {
                 oldval = newval;
-                return newval = handler.call ( obj, prop, oldval, val );
+                return newval = val === oldval ? val : handler.call ( obj, prop, oldval, val );
             };
-        if ( delete obj[ prop ] ) {
-            if ( Object.defineProperty )
-                Object.defineProperty ( obj, prop, {
-                    get: getter,
-                    set: setter
-                } );
-            else if ( Object.prototype.__defineGetter__ && Object.prototype.__defineSetter__ ) {
-                Object.prototype.__defineGetter__.call ( obj, prop, getter );
-                Object.prototype.__defineSetter__.call ( obj, prop, setter );
+        if ( Object.defineProperty )
+            Object.defineProperty ( obj, prop, {
+                get: getter,
+                set: setter
+            } );
+        else if ( Object.prototype.__defineGetter__ && Object.prototype.__defineSetter__ ) {
+            Object.prototype.__defineGetter__.call ( obj, prop, getter );
+            Object.prototype.__defineSetter__.call ( obj, prop, setter );
+        }
+        if ( Fancy.isArray ( obj[ prop ] ) ) {
+            for ( var i = 0; i < obj[ prop ].length; i++ ) {
+                Fancy.watch ( obj[ prop ], i, handler );
+            }
+        } else if ( Fancy.isObject ( obj[ prop ] ) ) {
+            for ( var p in obj[ prop ] ) {
+                if ( obj[ prop ].hasOwnProperty ( p ) ) {
+                    Fancy.watch ( obj[ prop ], p, handler );
+                }
             }
         }
-        obj[ prop ] = oldval;
     };
-
-    Fancy.unwatch = function ( obj, prop ) {
+    Fancy.unwatch        = function ( obj, prop ) {
         var val     = obj[ prop ];
         delete obj[ prop ];
         obj[ prop ] = val;
     };
-
-    Fancy.scrollParent = function ( el ) {
+    Fancy.scrollParent   = function ( el ) {
         var position            = el.css ( "position" ),
             excludeStaticParent = position === "absolute",
             scrollParent        = el.prop ( 'nodeName' ) == "TEXTAREA" && el [ 0 ].scrollHeight - el.outerHeight () > 0 ? el : false;
@@ -208,45 +271,30 @@
             } ).eq ( 0 );
         return position === "fixed" || !scrollParent.length ? $ ( el [ 0 ].ownerDocument || document ) : scrollParent;
     };
+    Fancy.settings       = {};
+    Fancy.api.set        = function ( name, fn ) {
+        var instance;
 
-    Fancy.settings = {};
-
-    function construct ( constructor, args ) {
-        function API () {
-            return constructor.apply ( this, args );
-        }
-
-        API.prototype = constructor.prototype;
-        return new API ();
-    }
-
-    Fancy.api.set = function ( api ) {
-        var args = [],
-            instance;
-        for ( var a = 1; a < arguments.length; a++ ) {
-            args.push ( arguments[ a ] );
-        }
         if ( this.element.length ) {
-            var data = this.get ( api.prototype.name );
+            var data = this.get ( name );
             if ( data && data.length ) {
                 for ( var i = 0; i < data.length; i++ ) {
                     if ( typeof data [ i ] == "undefined" ) {
-                        instance   = construct ( $ ( this.element [ i ] ), args );
+                        instance   = fn ( $ ( this.element [ i ] ) );
                         data [ i ] = instance;
-                        $ ( this.element [ i ] ).data ( instance.name, instance );
+                        $ ( this.element [ i ] ).data ( name, instance );
                     }
                 }
             }
             if ( !data ) {
-                instance = construct ( this.element, args );
-                this.element.data ( instance.name, instance );
+                instance = fn ( this.element );
+                this.element.data ( name, instance );
                 return instance;
             } else
                 return data;
         }
     };
-
-    Fancy.api.get = function ( name ) {
+    Fancy.api.get        = function ( name ) {
         if ( name.indexOf ( 'Fancy' ) != 0 ) {
             if ( typeof this [ name ] == "function" ) {
                 name = "Fancy" + name [ 0 ].toUpperCase () + name.slice ( 1, name.length );
@@ -267,6 +315,48 @@
         }
         return this.element.data ( name );
     };
+    Fancy.api.fullHeight = function ( mrgn ) {
+        var pddng = true, brdr = true;
+        if ( typeof mrgn === "object" ) {
+            pddng = mrgn.padding != false;
+            brdr  = mrgn.border != false;
+        }
+        return this.height ()
+            + (pddng ? parseInt ( this.css ( "paddingTop" ) ) : 0)
+            + (pddng ? parseInt ( this.css ( "paddingBottom" ) ) : 0)
+            + (brdr ? parseInt ( this.css ( "borderBottomWidth" ) ) : 0)
+            + (brdr ? parseInt ( this.css ( "borderTopWidth" ) ) : 0)
+            + (mrgn ? parseInt ( this.css ( "marginTop" ) ) + parseInt ( this.css ( "marginBottom" ) ) : 0);
+    };
+    jQuery.fn.fullWidth  = function ( mrgn ) {
+        var pddng = true, brdr = true;
+        if ( typeof mrgn === "object" ) {
+            pddng = mrgn.padding != false;
+            brdr  = mrgn.border != false;
+        }
+        return this.width ()
+            + (pddng ? parseInt ( this.css ( "paddingLeft" ) ) : 0)
+            + (pddng ? parseInt ( this.css ( "paddingRight" ) ) : 0)
+            + (brdr ? parseInt ( this.css ( "borderLeftWidth" ) ) : 0)
+            + (brdr ? parseInt ( this.css ( "borderRightWidth" ) ) : 0)
+            + (mrgn ? parseInt ( this.css ( "marginLeft" ) ) + parseInt ( this.css ( "marginRight" ) ) : 0);
+    };
+    Fancy.getKey         = function ( o, s ) {
+        s = s.replace ( /\[(\w+)\]/g, '.$1' ); // convert indexes to properties
+        s = s.replace ( /^\./, '' ); // strip a leading dot
+        var a = s.split ( '.' );
+        for ( var i = 0; i < a.length; i++ ) {
+            var k = a[ i ];
+
+            if ( o.hasOwnProperty ( k ) ) {
+                o = o[ k ];
+            } else {
+                return;
+            }
+        }
+        return o;
+    };
 
     window.Fancy = Fancy;
+    Fancy.version ( Fancy.api );
 }) ( jQuery );
